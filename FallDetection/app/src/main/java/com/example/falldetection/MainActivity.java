703db1ffd242ac6668be0a61b280dd914bf69734
Sean Tang <1996sean@gmail.com>
2021-03-12 16:43:12 +0800

    Added base code for Flask backend functionality

--- FallDetection/app/src/main/java/com/example/falldetection/MainActivity.java
+++ FallDetection/app/src/main/java/com/example/falldetection/MainActivity.java
@@ -26,18 +26,82 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.text.DecimalFormat;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView displayTextview;
     final protected String TAG= "HULLO";
+    private String url = "http://" + "192.168.0.111:5000/";
+    private String emergencyContact= "emergencyalert?client=0";
+    private String postBodyString;
+    private MediaType mediaType;
+    private RequestBody requestBody;
+
+    protected RequestBody buildRequestBody(String msg){
+        postBodyString= msg;
+        mediaType = MediaType.parse("text/plain");
+        requestBody = RequestBody.create(postBodyString, mediaType);
+        return requestBody;
+
+    }
+
+    private void postRequest(String message, String URL) {
+        RequestBody requestBody = buildRequestBody(message);
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request
+                .Builder()
+                .post(requestBody)
+                .url(URL)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(final Call call, final IOException e) {
+                Log.i(TAG, "onFailure:"+e.getMessage());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Something went wrong:" + " " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        call.cancel();
+                    }
+                });
+
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Log.i(TAG, "onFailure: "+ response.body().string());
+                            Toast.makeText(MainActivity.this, response.body().string(), Toast.LENGTH_LONG).show();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+
+            }
+        });
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     protected void trackSteps(){
-        Log.i(TAG, "trackSteps: Checkpoint 1");
+
         SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
@@ -45,8 +109,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
                 if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-
-                    Log.i(TAG, "trackSteps: Checkpoint 2");
                     double loX = sensorEvent.values[0];
                     double loY = sensorEvent.values[1];
                     double loZ = sensorEvent.values[2];
@@ -59,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
                     double ldAccRound = Double.parseDouble(precision.format(loAccelerationReader));
 
                     if (ldAccRound > 0.3d && ldAccRound < 0.5d) {
-                        Log.i(TAG, "trackSteps: Checkpoint 3");
+
                         displayTextview.setText("Hi it works");
 
                         new CountDownTimer(5000,50) {
@@ -70,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
 
                             @Override
                             public void onFinish() {
+                                postRequest("your message here", url+emergencyContact);
                                 displayTextview.setText("Ready for the next fall");
                             }
                         }.start();
@@ -85,6 +148,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -96,6 +160,7 @@ public class MainActivity extends AppCompatActivity {
 
         displayTextview= findViewById(R.id.display_text);
         trackSteps();
+        postRequest("your message here", url+emergencyContact);
     }
 
 
